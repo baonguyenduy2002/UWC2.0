39ae4cdee76ed5148e95e4fdd510086ad9ab01d0
talu <talu.dev@gmail.com>
2022-12-01 15:55:22 +0700

    feat: finish login ui

--- mvc/src/pages/LoginPage/index.jsx
+++ mvc/src/pages/LoginPage/index.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import login_background from "../../assets/image/login_background.svg"
+import login_background from "../../assets/image/login_background.svg";
 import "./LoginPage.css";
+import { login } from "../../mock/controller/auth";
 
 function LoginPage() {
+	const navigate = useNavigate();
+
+	const [userId, setUserId] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = async () => {
+		login(userId, password);
+		setTimeout(navigate("/dashboard"), 2000);
+	};
+
 	return (
-	<div className="container">
-		<h1>Sign in</h1>
-		<p>Sign in and start managing your employee!</p>
-		<form className="form">
-  			<input type="text" id="fname" name="fname" placeholder="User ID"/>
-  			<input type="text" id="lname" name="lname" placeholder="Password"/>
-			<div>
-				<input type="checkbox" />
-				<p>Remember me</p>
-			</div>
-  			<button>Login</button>
-		</form>
-		<img src= {login_background} alt="Login Background" className="login_background"/>
-	</div>
+		<div className="container">
+			<h1 className="title">Sign in</h1>
+			<p className="normal-text">Sign in and start managing your employee!</p>
+
+			<form className="form">
+				<input
+					className="form-input"
+					placeholder="User ID"
+					onChange={(e) => setUserId(e.target.value)}
+				/>
+				<input
+					className="form-input"
+					type="password"
+					placeholder="Password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<div className="remember-me">
+					<input className="form-checkbox" type="checkbox" />
+					<p className="normal-text">Remember me</p>
+				</div>
+				<button type="button" className="form-button" onClick={handleSubmit}>
+					Login
+				</button>
+			</form>
+			<img
+				src={login_background}
+				alt="Login Background"
+				className="login_background"
+			/>
+		</div>
 	);
 }
 
