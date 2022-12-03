31808bb344bccb7f62392fbf3792b5aa27d2190b
mynamesbao11 <bao.nguyenduy2002@hcmut.edu.vn>
2022-12-04 02:00:34 +0700

    Back-End: Refactor folders and first step in back-end. 2g00 4/12/2022

--- mvc/src/components/ManageTaskwindow/ManageTask.jsx
+++ mvc/src/components/ManageTaskwindow/ManageTask.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
+import { getTask, getTasks } from "../../controller/api/crudtask";
 import "./ManageTask.css";
 
 const ManageTask = () => {
-  return <div>ManageTask</div>;
+	const [taskChosen, setTaskChosen] = useState([]);
+	const [taskList, setTaskList] = useState({});
+
+	useEffect(() => {
+		// getTask("0001").then((res) => setTaskChosen(res));
+		getTasks().then((res) => setTaskList(res));
+		console.log(taskList);
+	}, []);
+
+	return (
+		<div className="task">
+			<h1>
+				{/* {taskList.map((task) => {
+					return <p>{task.toString()}</p>;
+				})} */}
+			</h1>
+		</div>
+	);
 };
 
 export default ManageTask;
