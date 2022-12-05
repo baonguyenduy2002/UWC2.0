d2a6d872662e6655e938abd8f3d09f26690109ed
Danh Nguyen <dd-devq@gmail.com>
2022-12-06 01:46:55 +0700

    Chat

--- mvc/src/components/ChatWindow/ChatWindow.jsx
+++ mvc/src/components/ChatWindow/ChatWindow.jsx
@@ -6,8 +6,18 @@ import "./ChatWindow.css";
 import Rimsha from "../../assets/image/Rimsha64x64.png";
 import info from "../../assets/icons/info.svg"
 import send from "../../assets/icons/send.svg"
+import { useRef } from "react";
 const ChatWidnow = () => {
+    const [message, setMessage] = useState('');
+    const inputRef = useRef(null);
+    let msg = []
+    function handleClick() {
+        setMessage('');
+    }
 
+    const handleChange = event => {
+        setMessage(event.target.value);
+    };
     return (
         <div className="ChatWindow">
             <div className="ChatFriend">
@@ -19,16 +29,29 @@ const ChatWidnow = () => {
             </div>
 
             <div className="ChatView">
+                <img className="Avatar1" src={Rimsha} width={40} height={40}></img>
+                <div className="ChatView-msg msg1">
+                    Lorem ipsum dolor sit amet, con.
+                </div>
 
+                <div className="ChatTime">
+                    19:20
+                </div>
+
+                <img className="Avatar2" src={Rimsha} width={40} height={40}></img>
+                <div className="ChatView-msg msg2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum sodales nisl, eget ultricies diam pharetra vel. Vivamus lobortis eu mauris ac vehicula. Suspendisse eget.
+                </div>
             </div>
 
             <div className="ChatInput">
                 <div className="ChatInput-field">
-                    <div className="ChatInput-field-default-text">
-                        Nhập tin nhắn . . .
-                    </div>
+                    <input className="ChatInput-input-field" ref={inputRef} type="text" placeholder="Nhập tin nhắn . . ." onChange={handleChange} value={message}>
+                    </input>
                 </div>
-                <img className="SendIcon" src={send}></img>
+                <div className="SendIcon-container" onClick={handleClick}>
+                    <img className="SendIcon" src={send}></img>
+                </div>
             </div>
         </div>
     );
