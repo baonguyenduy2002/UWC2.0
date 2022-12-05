787e03459653a5fab075edaf6617f543414c2be8
Danh Nguyen <dd-devq@gmail.com>
2022-12-05 10:21:17 +0700

    Finish Setting

--- mvc/src/components/SettingGeneral/SettingGeneral.jsx
+++ mvc/src/components/SettingGeneral/SettingGeneral.jsx
@@ -5,7 +5,29 @@ import "./SettingGeneral.css";
 import { logout } from "../../mock/controller/auth";
 import { useNavigate } from "react-router-dom";
 import Logout from "../../assets/icons/logout.svg"
+import open from "../../assets/icons/arrow_drop_down_open.svg"
+import close from "../../assets/icons/arrow_drop_down_close.svg"
+
 const SettingGeneral = () => {
+
+    const [isShown, setIsShown] = useState(false)
+    const [message, setMessage] = useState('Tiếng Việt');
+    const [icon, setIcon] = useState(open)
+
+    const handleLanguageClick = () => {
+        setIsShown(current => !current)
+        if (icon == close) {
+            setIcon(open)
+        }
+        else {
+            setIcon(close)
+        }
+    }
+
+    function handleLangChoose(lang) {
+        setMessage(lang)
+        setIsShown(current => !current)
+    }
 
     const navigate = useNavigate();
 
@@ -23,11 +45,13 @@ const SettingGeneral = () => {
                 <div className="GHeader1">
                     Ngôn Ngữ
                 </div>
-                <select className="Dropdown">
-                    <option value="Vietnamese">Tiếng Việt</option>
-                    <option value="English">English</option>
-                </select>
-            </div>
+                <button className="Dropdown" onClick={handleLanguageClick} >{message}</button>
+                {isShown && (<div className="Dropdown-list">
+                    <button className="Dropdown-list-item item1" onClick={() => { handleLangChoose('Tiếng Việt') }} >Tiếng Việt</button>
+                    <button className="Dropdown-list-item item2" onClick={() => { handleLangChoose('English') }}>English</button>
+                </div>)}
+                <img className="ImgDropdown" src={icon}></img>
+            </div >
 
             <div className="Logout">
                 <div className="GHeader2">
@@ -40,7 +64,7 @@ const SettingGeneral = () => {
                 </div>
                 <img className="LogoutIcon" src={Logout}></img>
             </div>
-        </div>
+        </div >
     );
 };
 
