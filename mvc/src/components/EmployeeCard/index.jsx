158a9338605da75d99f7ff3ed578ce4f6ab59a2a
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2022-12-05 22:32:18 +0700

    Employee Management Windows

--- mvc/src/components/EmployeeCard/index.jsx
+++ mvc/src/components/EmployeeCard/index.jsx
@@ -7,19 +7,22 @@ function EmployeeCard(props) {
     return (
         <div className="CardContainer">
             <div className="Photo">
-                <img src={url} alt="photo" width="100px" height="120px"></img>
+                <img src={url} alt=""></img>
             </div>
-            <h3 className="EmployeeName" >{name}</h3>
-            <p className="EmployeeType">{type}</p>
-            <p className="EmployeeId">#{id}</p>
-            <div className="WorkingStatus">Status: {workingStatus ? 'Working':"Not Working"}</div>
-            <div className="DoB">
-                <p id="DoBContent"><b>Working Area:</b> {area}</p>
+            <div className="BasicInfo">
+                <h3 style={{ fontSize: 20, margin: 0, width: 'fit-content' }}>{name}</h3>
+                <p style= {{ fontSize: 20, margin: 0, width: 'fit-content' }}>{type}</p>
+                <p style= {{ fontSize: 20, margin: 0, width: 'fit-content' }}><b>ID:</b> #{id}</p>
+                <p style= {{ fontSize: 20, margin: 0, width: 'fit-content' }}>{workingStatus ? 'Working':"Not Working"}</p>
             </div>
-            <div className="Address">
-                <p id="AddressContent"><b>Address:</b> {address}</p>
+            <div className="CurrentTaskTitle">
+                <h4 style={{margin: 0, fontSize: '20px', width: 'fit-content' }}>Working Area:</h4>
+                <h4 style={{margin: 0, fontSize: '20px', width: 'fit-content' }}>Vehicle:</h4>
             </div>
-            
+            <div className="CurrentTaskContent">
+                <p style={{margin: 0, fontSize: '20px', width: 'fit-content' }}>Area 1</p>
+                <p style={{margin: 0, fontSize: '20px', width: 'fit-content' }}>#CH12</p>
+            </div>
         </div>
     );
 }
