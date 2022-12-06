8c377ec2ea140c686345d54d090f39491a2419f5
mynamesbao11 <bao.nguyenduy2002@hcmut.edu.vn>
2022-12-06 12:25:03 +0700

    nothing new

--- mvc/src/components/PersonalInfo/index.jsx
+++ mvc/src/components/PersonalInfo/index.jsx
@@ -4,43 +4,57 @@ import { iconsEmployee } from "../../assets/index";
 
 import "./PersonalInfo.css";
 
-
 function PersonalInfo(props) {
-    const {id, name, type, workingStatus, dob, address, email, url} = props;
-    return (
-        <div className="PersonalInfoContainer">
-            <div className="Part1">
-                <div className="PersonalImage">
-                    <img src={url} alt="PersonalImage" width="214px" height="231px" />
-                </div>
-                <div className="Info-1">
-                    <h1 style={{
-                            margin: 0,
-                            fontSize: '40px',
-                            width: 'fit-content',
-                        }}
-                    >
-                        {name}
-                    </h1>
-                    <p id="StaffID"><b>ID: </b>{id}</p>
-                    <p id="StaffType"><b>Chức vụ: </b>{type}</p>
-                    <p id="StaffStatus"><b>Tình trạng: </b>{workingStatus? 'Đang làm việc':'Tạm nghĩ'}</p>
-                </div>
-                <div className="RemoveEmployee-Button">
-                    <RemoveEmployeeButton item={iconsEmployee[1]} />
-                </div>
-            </div>
-            <div className="Part2">
-                <div><b>Date of Birth:</b>{dob}</div>
-                <div><b>Address:</b>{address}</div>
-                <div><b>Email:</b>{email}</div>
-            </div>
-            <div className="Part3">
-
-            </div>
-
-
+  const { id, name, type, workingStatus, dob, address, email, url } = props;
+  return (
+    <div className="PersonalInfoContainer">
+      <div className="Part1">
+        <div className="PersonalImage">
+          <img src={url} alt="PersonalImage" width="214px" height="231px" />
         </div>
-    );
+        <div className="Info-1">
+          <h1
+            style={{
+              margin: 0,
+              fontSize: "40px",
+              width: "fit-content",
+            }}
+          >
+            {name}
+          </h1>
+          <p id="StaffID">
+            <b>ID: </b>
+            {id}
+          </p>
+          <p id="StaffType">
+            <b>Chức vụ: </b>
+            {type}
+          </p>
+          <p id="StaffStatus">
+            <b>Tình trạng: </b>
+            {workingStatus ? "Đang làm việc" : "Tạm nghĩ"}
+          </p>
+        </div>
+        <div className="RemoveEmployee-Button">
+          <RemoveEmployeeButton item={iconsEmployee[1]} />
+        </div>
+      </div>
+      <div className="Part2">
+        <div>
+          <b>Date of Birth:</b>
+          {dob}
+        </div>
+        <div>
+          <b>Address:</b>
+          {address}
+        </div>
+        <div>
+          <b>Email:</b>
+          {email}
+        </div>
+      </div>
+      <div className="Part3"></div>
+    </div>
+  );
 }
 export default PersonalInfo;
