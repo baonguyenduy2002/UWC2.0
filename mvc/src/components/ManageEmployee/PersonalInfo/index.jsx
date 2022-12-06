acb578fc7ce4c60715d62076f21f816de4b26a8d
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2022-12-06 14:54:44 +0700

    Test PersonalInfo page

--- mvc/src/components/ManageEmployee/PersonalInfo/index.jsx
+++ mvc/src/components/ManageEmployee/PersonalInfo/index.jsx
@@ -1,48 +1,45 @@
-import React from "react";
-import RemoveEmployeeButton from "../RemoveEmployee/index";
-import { iconsEmployee } from "../../../assets/index";
+// import React from "react";
+// import RemoveEmployeeButton from "../RemoveEmployee/index";
+// import { iconsEmployee } from "../../../assets/index";
 
-import "./PersonalInfo.css";
+// import "./PersonalInfo.css";
 
-function PersonalInfo(props) {
-    const {id, name, type, workingStatus, dob, address, email, url} = props;
-    return (
-        <div className="PersonalInfoContainer">
-            <div className="Part1">
-                <div className="PersonalImage">
-                    <img src={url} alt="PersonalImage" width="214px" height="231px" />
-                </div>
-                <div className="Info-1">
-                    <h1 style={{
-                            margin: 0,
-                            fontSize: '40px',
-                            width: 'fit-content',
-                        }}
-                    >
-                        {name}
-                    </h1>
-                    <p id="StaffID"><b>ID: </b>{id}</p>
-                    <p id="StaffType"><b>Chức vụ: </b>{type}</p>
-                    <p id="StaffStatus"><b>Tình trạng: </b>{workingStatus? 'Đang làm việc':'Tạm nghĩ'}</p>
-                </div>
-                <div className="RemoveEmployee-Button">
-                    <RemoveEmployeeButton item={iconsEmployee[1]} />
-                </div>
-            </div>
-            <div className="Part2">
-                <div><b>Date of Birth:&nbsp;</b>{dob}</div>
-                <div><b>Address:&nbsp;</b>{address}</div>
-                <div><b>Email:&nbsp;</b>{email}</div>
-            </div>
-            <div className="Part3">
+// function PersonalInfo(props) {
+//     const {id, name, type, workingStatus, dob, address, email, url} = props;
+//     return (
+//         <div className="PersonalInfoContainer">
+//             <div className="Part1">
+//                 <div className="PersonalImage">
+//                     <img src={url} alt="PersonalImage" width="214px" height="231px" />
+//                 </div>
+//                 <div className="Info-1">
+//                     <h1 style={{
+//                             margin: 0,
+//                             fontSize: '40px',
+//                             width: 'fit-content',
+//                         }}
+//                     >
+//                         {name}
+//                     </h1>
+//                     <p id="StaffID"><b>ID: </b>{id}</p>
+//                     <p id="StaffType"><b>Chức vụ: </b>{type}</p>
+//                     <p id="StaffStatus"><b>Tình trạng: </b>{workingStatus? 'Đang làm việc':'Tạm nghĩ'}</p>
+//                 </div>
+//                 <div className="RemoveEmployee-Button">
+//                     <RemoveEmployeeButton item={iconsEmployee[1]} />
+//                 </div>
+//             </div>
+//             <div className="Part2">
+//                 <div><b>Date of Birth:&nbsp;</b>{dob}</div>
+//                 <div><b>Address:&nbsp;</b>{address}</div>
+//                 <div><b>Email:&nbsp;</b>{email}</div>
+//             </div>
+//             <div className="Part3">
 
-            </div>
+//             </div>
 
 
-        </div>
-      </div>
-      <div className="Part3"></div>
-    </div>
-  );
-}
-export default PersonalInfo;
+//         </div>
+//   );
+// }
+// export default PersonalInfo;
